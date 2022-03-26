7155bc83ae8907e4b797d4d0f428b4efabdd31c8
Christopher Callender <christophercallender0000@gmail.com>
2022-03-26 09:29:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+// describe('Fix the Scope', function() {
+//     describe('declare customerName to be bob in global scope', function() {
+//       it('returns the customerName', function() {
+//         expect(window.customerName).to.equal('bob');
+//       });
+//     });
+customerName = 'bob';
+//     describe('upperCaseCustomerName()', function() {
+//       it('modifies the customerName variable', function() {
+//         expect(window.customerName).to.equal('bob');
+//         upperCaseCustomerName();
+//         expect(window.customerName).to.equal('BOB');
+//       });
+//     });
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+//     describe('setBestCustomer()', function() {
+//       it('setBestCustomer', function() {
+//         expect(window.bestCustomer).to.equal(undefined);
+//         setBestCustomer();
+//         expect(window.bestCustomer).to.equal('not bob');
+//       });
+//     });
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+//     describe('overwriteBestCustomer()', function() {
+//       it('overwrites the best customer', function() {
+//         overwriteBestCustomer('maybe bob');
+//         expect(window.bestCustomer).to.equal('maybe bob');
+//       });
+//     });
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+//     describe('changeLeastFavoriteCustomer()', function() {
+//       it('unsuccessfully tries to reassign the least favorite customer', function() {
+//         expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+//       });
+//     });
+const leastFavoriteCustomer = 'original value';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new value'
+}
+//   });
